6c28523eab48de88058872de13154fb704f6d597
Adam Ryman <adamr@tune.com>
2016-09-23 15:53:39 -0700

    Docker compose now launches mysql

--- ambition/envscript/createTables.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE actions(id SERIAL PRIMARY KEY, action_name varchar(255), set_id integer, user_id integer)
-CREATE TABLE occurrences(id SERIAL PRIMARY KEY, action_id integer, time timestamp)
