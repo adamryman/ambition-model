1f420c709ded44ce9d01e8e5434cba26fe43ec1f
Adam Ryman <adamryman@gmail.com>
2017-01-28 18:11:30 -0800

    Improve database interface comments

--- ambition-service/database.go
+++ ambition-service/database.go
@@ -4,12 +4,11 @@ package ambition
 type Database interface {
 	// CreateAction requires Action.Name and Action.UserID
 	CreateAction(in *Action) (*Action, error)
-	// CreateOccurrence requires CreateOccurrenceRequest.UserID ,
-	// .Occurrence.ActionId, and Occurrence.Datetime
+	// CreateOccurrence requires Occurrence.ActionId, and Occurrence.Datetime
 	CreateOccurrence(in *Occurrence) (*Occurrence, error)
-	// CreateAction requires Action.Name and Action.UserID
+	// ReadActionByID requires all inputs
 	ReadActionByID(id int64) (*Action, error)
-	// CreateAction requires Action.Name and Action.UserID
+	// ReadActionByNameAndUserID requires all inputs
 	ReadActionByNameAndUserID(name string, userId int64) (*Action, error)
 	// TODO: Add queries for all rpc's
 }
